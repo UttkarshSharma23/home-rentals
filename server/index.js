65c7ea74bd3458bb88b153a804d5c70f6db58fe5
UttkarshSharma23 <uttkarsh.sharma2019@gmail.com>
2024-02-25 01:33:45 +0530

    update

--- server/index.js
+++ server/index.js
@@ -4,6 +4,24 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
+
+//Handling the CORS policy issue: Giving access to CORS and acting as a middleware
+const corsOptions = {
+  origin:"http://localhost:3000",
+  methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentials:true,
+}
+app.use(cors(corsOptions));
+
+/* MONGOOSE SETUP */
+mongoose
+  .connect('mongodb+srv://uttkarshsharma:EmkghhrQRLg9Batn@home.n8go9wm.mongodb.net/?retryWrites=true&w=majority&appName=home')
+  .then(
+    console.log("MongoDB Connected Successfully!")
+  )
+  .catch((error) => console.log("Error connecting to MongoDB", error));
+
+
 const authRoutes = require("./routes/auth.js")
 const listingRoutes = require("./routes/listing.js")
 const bookingRoutes = require("./routes/booking.js")
@@ -19,15 +37,10 @@ app.use("/properties", listingRoutes)
 app.use("/bookings", bookingRoutes)
 app.use("/users", userRoutes)
 
-/* MONGOOSE SETUP */
-const PORT = 3001;
-mongoose
-  .connect(process.env.MONGO_URL, {
-    dbName: "Dream_Nest",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-  })
-  .catch((err) => console.log(`${err} did not connect`));
+
+
+  const PORT = 3001;
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
